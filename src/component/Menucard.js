c49ec36cee2b189ecea3cce83ec90fb73c3f9b0d
Sandip7677 <anuj7870040353@gmail.com>
2023-06-13 12:03:15 +0530

    searching functionality addeed

--- src/component/Menucard.js
+++ src/component/Menucard.js
@@ -1,7 +1,39 @@
 import React from "react";
 
-function Menucard() {
-  return <></>;
+function Menucard({ recipi }) {
+  return (
+    <>
+      <section className="main-card--cointainer">
+        {recipi.map((cur, ind) => {
+          const { label, image, shareAs, cautions, mealType } = cur.recipe;
+          return (
+            <>
+              <div className="card-container" key={ind}>
+                <div className="card ">
+                  <div className="card-body">
+                    <span className="card-number card-circle subtle">
+                      {ind}
+                    </span>
+                    <span className="card-author subtle"> {mealType[0]}</span>
+                    <h2 className="card-title"> {label} </h2>
+                    <span className="card-description subtle">
+                      {cautions[0]}
+                    </span>
+                    <div className="card-read">Read</div>
+                  </div>
+                  <img src={image} alt="images" className="card-media" />
+
+                  <span className="card-tag  subtle">
+                    <a href={shareAs}>Find recipe</a>
+                  </span>
+                </div>
+              </div>
+            </>
+          );
+        })}
+      </section>
+    </>
+  );
 }
 
 export default Menucard;
--- src/component/Resturant.js
+++ src/component/Resturant.js
@@ -5,10 +5,10 @@ import Menucard from "./Menucard";
 
 const Resturant = () => {
   const [recipi, setRecipi] = useState([]);
-  const [menuList, setMenuList] = useState([]);
+  const [searchdata, setSearchdata] = useState("chicken");
   const getdata = async () => {
     const recipe = await axios.get(
-      "https://api.edamam.com/api/recipes/v2?type=public&q=chicken&app_id=9223cf9b&app_key=8daf0aae98ec03149759b6398bbf3413"
+      `https://api.edamam.com/api/recipes/v2?type=public&q=${searchdata}&app_id=9223cf9b&app_key=8daf0aae98ec03149759b6398bbf3413`
     );
     setRecipi(recipe.data.hits);
     console.log(recipe.data.hits);
@@ -19,6 +19,20 @@ const Resturant = () => {
 
   return (
     <>
+      <div className="nav d-flex">
+        <input
+          type="text"
+          placeholder="search.."
+          className="search"
+          value={searchdata}
+          onChange={(e) => {
+            setSearchdata(e.target.value);
+          }}
+        />
+        <button className="button" onClick={getdata}>
+          search
+        </button>
+      </div>
       <Menucard recipi={recipi} />
     </>
   );
